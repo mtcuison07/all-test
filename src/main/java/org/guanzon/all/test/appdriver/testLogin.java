ce86d859795b67cd6f5d0c2d25f88a5b54e70417
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2024-04-10 09:21:53 +0800

    Initial commit

---
+++ src/main/java/org/guanzon/all/test/appdriver/testLogin.java
@@ -0,0 +1,25 @@
+package org.guanzon.all.test.appdriver;
+
+import org.guanzon.appdriver.base.GRider;
+
+public class testLogin {
+    public static void main(String[] args) {
+        String path;
+        if(System.getProperty("os.name").toLowerCase().contains("win")){
+            path = "D:/GGC_Java_Systems";
+        }
+        else{
+            path = "/srv/GGC_Java_Systems";
+        }
+        System.setProperty("sys.default.path.config", path);
+        
+        GRider instance = new GRider("gRider");
+        
+        if (!instance.logUser("gRider", "M001000001")){
+            System.err.println(instance.getErrMsg());
+            System.exit(1);
+        }
+        
+        System.out.println("Connected");
+    }
+}
